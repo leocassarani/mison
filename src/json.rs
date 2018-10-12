147993d236923194c9593b5094eb44b832c6c7a7
Leo Cassarani <me@leocassarani.net>
2018-10-12 22:38:28 +0100

    Parse simple JSON values

--- src/json.rs
+++ src/json.rs
@@ -1,7 +1,45 @@
+use std::str;
+
 #[derive(Debug)]
 pub enum Value {
     Null,
     Bool(bool),
     Number(f64),
     String(String),
+}
+
+impl Value {
+    pub fn parse(bytes: &[u8]) -> Option<Self> {
+        if bytes.is_empty() {
+            return None;
+        }
+
+        if bytes[0] == b'"' {
+            Self::parse_string(&bytes[1..])
+        } else if (bytes[0] >= b'0' && bytes[0] <= b'9') || bytes[0] == b'-' {
+            Self::parse_number(bytes)
+        } else if bytes.len() >= 4 && bytes == b"null" {
+            Some(Value::Null)
+        } else if bytes.len() >= 4 && bytes == b"true" {
+            Some(Value::Bool(true))
+        } else if bytes.len() >= 5 && bytes == b"false" {
+            Some(Value::Bool(false))
+        } else {
+            None
+        }
+    }
+
+    fn parse_number(bytes: &[u8]) -> Option<Self> {
+        let delimiter = bytes.iter().position(|&ch| ch == b',' || ch == b'}')?;
+        let string = str::from_utf8(&bytes[..delimiter]).ok()?;
+        string.parse().map(Value::Number).ok()
+    }
+
+    fn parse_string(bytes: &[u8]) -> Option<Self> {
+        let delimiter = bytes.iter().position(|&ch| ch == b'"')?;
+        str::from_utf8(&bytes[..delimiter])
+            .map(|s| s.to_string())
+            .map(Value::String)
+            .ok()
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,9 @@ pub struct Query {
 impl Query {
     pub fn new(fields: Vec<impl Into<String>>) -> Self {
         let mut field_set = HashSet::with_capacity(fields.len());
-
         for field in fields {
             field_set.insert(field.into());
         }
-
         Query { field_set }
     }
 
@@ -33,7 +31,6 @@ pub struct Record {
 impl Record {
     fn new(bytes: Vec<u8>, fields: HashSet<String>) -> Self {
         let colons = ColonBitmaps::generate(&bytes);
-
         Record {
             bytes,
             fields,
@@ -45,29 +42,59 @@ impl Record {
     fn key_preceding(&self, colon: usize) -> Option<String> {
         let mut start = 0;
         let mut end = 0;
-        let mut inside = false;
 
+        // Skip all whitespace until we find the index of the closing quote.
         for i in (0..colon).rev() {
-            if self.bytes[i] == b'"' {
-                if inside {
-                    start = i + 1; // TODO: check if safe
-                    break;
-                } else {
-                    inside = true;
+            match self.bytes[i] {
+                b'"' => {
                     end = i;
+                    break;
+                }
+                b'\t' | b'\n' | b'\r' | b' ' => {}
+                _ => {
+                    return None;
                 }
             }
         }
 
-        if start >= end {
+        // Keep going backwards until we find the index of the opening quote.
+        for i in (0..end).rev() {
+            if self.bytes[i] == b'"' {
+                // If we've found a quote, we need to check if it's preceded by
+                // a backslash character, which would mean it's an escaped quote
+                // in the middle of the string rather than a structural quote.
+                if i > 0 && self.bytes[i - 1] == b'\\' {
+                    // TODO: check if the backslash is itself preceded by a backslash.
+                    // If there is an odd number of backslashes, then we need to skip
+                    // this quote, otherwise it is in fact the opening quote.
+                    continue;
+                }
+
+                // The string starts at the character following the opening quote.
+                // In the case of an empty string, start will equal end, which will
+                // correctly produce an empty range.
+                start = i + 1;
+                break;
+            }
+        }
+
+        // If we failed to find an opening quote, or if we somehow ended up with
+        // an invalid range, then we failed to parse this key.
+        if start == 0 || start > end {
             return None;
         }
 
         String::from_utf8(self.bytes[start..end].to_vec()).ok()
     }
 
-    fn value_following(&self, _colon: usize) -> Option<Value> {
-        Some(Value::Null)
+    fn value_following(&self, colon: usize) -> Option<Value> {
+        let start = colon + 1;
+
+        if start < self.bytes.len() {
+            Value::parse(&self.bytes[start..])
+        } else {
+            None
+        }
     }
 }
 
@@ -85,12 +112,12 @@ impl Iterator for Record {
             }
 
             let colon = self.colons.indices[self.pos];
-            let key = self.key_preceding(colon)?;
-            let value = self.value_following(colon)?;
-
             self.pos += 1;
 
+            let key = self.key_preceding(colon)?;
+
             if self.fields.remove(&key) {
+                let value = self.value_following(colon)?;
                 return Some((key, value));
             }
         }
