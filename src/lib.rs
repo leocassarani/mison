5880c2622563587b9c0a8b332b889f48d797537c
Leo Cassarani <me@leocassarani.net>
2018-10-12 18:32:31 +0100

    Parse the key preceding each colon

--- src/lib.rs
+++ src/lib.rs
@@ -3,39 +3,113 @@ use std::collections::HashSet;
 
 mod json;
 
-pub struct Query<'a> {
-    field_set: HashSet<&'a str>,
+pub struct Query {
+    field_set: HashSet<String>,
 }
 
-impl<'a> Query<'a> {
-    pub fn new(fields: Vec<&'a str>) -> Self {
+impl Query {
+    pub fn new(fields: Vec<impl Into<String>>) -> Self {
         let mut field_set = HashSet::with_capacity(fields.len());
 
         for field in fields {
-            field_set.insert(field);
+            field_set.insert(field.into());
         }
 
         Query { field_set }
     }
 
     pub fn run(&self, json: String) -> Record {
+        Record::new(json.into_bytes(), self.field_set.clone())
+    }
+}
+
+pub struct Record {
+    bytes: Vec<u8>,
+    fields: HashSet<String>,
+    colons: ColonBitmaps,
+    pos: usize,
+}
+
+impl Record {
+    fn new(bytes: Vec<u8>, fields: HashSet<String>) -> Self {
+        let colons = ColonBitmaps::generate(&bytes);
+
         Record {
-            bytes: json.into_bytes(),
-            fields: self.field_set.clone(),
+            bytes,
+            fields,
+            colons,
+            pos: 0,
+        }
+    }
+
+    fn key_preceding(&self, colon: usize) -> Option<String> {
+        let mut start = 0;
+        let mut end = 0;
+        let mut inside = false;
+
+        for i in (0..colon).rev() {
+            if self.bytes[i] == b'"' {
+                if inside {
+                    start = i + 1; // TODO: check if safe
+                    break;
+                } else {
+                    inside = true;
+                    end = i;
+                }
+            }
+        }
+
+        if start >= end {
+            return None;
+        }
+
+        String::from_utf8(self.bytes[start..end].to_vec()).ok()
+    }
+
+    fn value_following(&self, _colon: usize) -> Option<Value> {
+        Some(Value::Null)
+    }
+}
+
+impl Iterator for Record {
+    type Item = (String, Value);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.fields.is_empty() {
+                return None;
+            }
+
+            if self.pos >= self.colons.len() {
+                return None;
+            }
+
+            let colon = self.colons.indices[self.pos];
+            let key = self.key_preceding(colon)?;
+            let value = self.value_following(colon)?;
+
+            self.pos += 1;
+
+            if self.fields.remove(&key) {
+                return Some((key, value));
+            }
         }
     }
 }
 
-pub struct Record<'a> {
-    bytes: Vec<u8>,
-    fields: HashSet<&'a str>,
+struct ColonBitmaps {
+    indices: Vec<usize>,
 }
 
-impl<'a> Iterator for Record<'a> {
-    type Item = (&'a str, Value);
+impl ColonBitmaps {
+    fn generate(_bytes: &[u8]) -> Self {
+        ColonBitmaps {
+            indices: vec![14, 46, 84, 97, 124, 142, 161, 182, 208, 231],
+        }
+    }
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.fields.take("name").map(|key| (key, Value::Null))
+    fn len(&self) -> usize {
+        self.indices.len()
     }
 }
 
