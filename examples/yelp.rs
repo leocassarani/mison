b0785042cbc88e8f9fef02a0cc3ad166dac8bdc7
Leo Cassarani <me@leocassarani.net>
2018-10-14 22:39:11 +0100

    Handle strings and numbers in the Yelp example file

--- examples/yelp.rs
+++ examples/yelp.rs
@@ -1,6 +1,6 @@
 extern crate mison;
 
-use mison::Query;
+use mison::{Query, Value};
 
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
@@ -11,7 +11,11 @@ fn main() -> Result<()> {
 
     for record in BufReader::new(file).lines() {
         for (key, value) in query.run(record?) {
-            println!("{}: {:?}", key, value);
+            match value {
+                Value::String(s) => println!("{}: {}", key, s),
+                Value::Number(n) => println!("{}: {}", key, n),
+                _ => println!("{}: {:?}", key, value),
+            }
         }
     }
 
